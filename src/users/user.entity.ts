d4fe9b4f3f3b23ff189044db079483535015b8f2
bhabin khadka <bhabin@bhabin-mac.local>
2022-07-12 10:29:52 +0545

    first commit

---
+++ src/users/user.entity.ts
@@ -0,0 +1,27 @@
+import { AfterInsert, AfterUpdate, AfterRemove, Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @AfterInsert()
+    logInsert() {
+        console.log('Insert user with id', this.id)
+    }
+    @AfterUpdate()
+    logUpdate() {
+        console.log('Update user with id', this.id)
+    }
+    @AfterRemove()
+    logRemove() {
+        console.log('Insert user with id', this.id)
+    }
+
+}
